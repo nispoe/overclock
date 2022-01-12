45fa5cf02f7ee48471e814ab45bbeef175e30749
nispoe <nispoe@eopsin.com>
2022-01-12 03:07:18 -0600

    Add OneDrive uninstall

--- UninstallPackages.ps1
+++ UninstallPackages.ps1
@@ -48,3 +48,6 @@ foreach ($ProvisionedAppName in $ProvisionedAppPackageNames) {
     Get-AppxPackage -Name $ProvisionedAppName -AllUsers | Remove-AppxPackage
     Get-AppXProvisionedPackage -Online | Where-Object DisplayName -EQ $ProvisionedAppName | Remove-AppxProvisionedPackage -Online
 }
+
+taskkill /f /im OneDrive.exe
+%SystemRoot%\SysWOW64\OneDriveSetup.exe /uninstall
