558563dc45242be04ee5601ea322d901796144b5
nispoe <nispoe@eopsin.com>
2022-01-12 16:32:37 -0600

    added eecution policy to update windows script

--- UpdateWindows.ps1
+++ UpdateWindows.ps1
@@ -1,4 +1,5 @@
 Install-PackageProvider NuGet -Force
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 Install-Module PSWindowsUpdate -Repository PSGallery
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force
 Get-WindowsUpdate -AcceptAll -Install -AutoReboot
