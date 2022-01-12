60679e45a74f4eceac7bbf6cd985c876af9ebd1d
nispoe <nispoe@eopsin.com>
2022-01-12 00:38:36 -0600

    test

--- first_script.ps1
+++ first_script.ps1
@@ -1,1 +1,4 @@
-Write-Host "Congratulations! Your first script executed successfully 1"
+Write-Host "Applications installed with the operating system"
+Get-AppxPackage | Select-Object Name
+Write-Host "Applications installed as part of the user profile first time setup"
+Get-AppxProvisionedPackage -Online | Select-object DisplayName
