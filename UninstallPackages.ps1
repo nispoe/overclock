68f5626a96f339f0b77d0b1ef75ae42c7d3d73ca
nispoe <nispoe@eopsin.com>
2022-01-12 02:54:59 -0600

    Update packages list

--- UninstallPackages.ps1
+++ UninstallPackages.ps1
@@ -42,11 +42,6 @@ $ProvisionedAppPackageNames = @(
     "Microsoft.Windows.Photos"
     "Microsoft.WindowsStore"
     "Microsoft.Xbox.TCUI"
-    "Microsoft.XboxGameCallableUI"
-    "Windows.PrintDialog"
-    "Microsoft.Windows.NarratorQuickStart"
-    "Microsoft.BioEnrollment"
-    ""
 )
 
 foreach ($ProvisionedAppName in $ProvisionedAppPackageNames) {
