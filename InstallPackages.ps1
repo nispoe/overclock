152befb1b358fe53e1f6a60dc1e1c31d116234dc
nispoe <nispoe@eopsin.com>
2022-01-12 19:17:05 -0600

    update how the install packages directory is created

--- InstallPackages.ps1
+++ InstallPackages.ps1
@@ -1,3 +1,4 @@
 $nispoepath = "C:\nispoe"
+New-Item -ItemType "directory" -Path $nispoepath
 Invoke-WebRequest -Uri "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" -OutFile $nispoepath"\SteamSetup.exe"
 Start-Process -FilePath $nispoepath"\SteamSetup.exe" -ArgumentList "/S"
