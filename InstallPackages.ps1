5d2883c7f11f8de3d5328f046ea62248b2d96220
nispoe <nispoe@eopsin.com>
2022-01-13 02:47:09 -0600

    correcting string concatinations in install packages

--- InstallPackages.ps1
+++ InstallPackages.ps1
@@ -6,12 +6,12 @@ if (!(Test-Path $nispoePath)) {
 
 # Install Steam (This is where I have my 3DMark and EVGA Precision X1 software)
 if (!(Test-Path "$nispoePath\SteamSetup.exe")) {
-    Invoke-WebRequest -Uri "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" -OutFile $nispoePath"\SteamSetup.exe"
-    Start-Process -FilePath $nispoePath"\SteamSetup.exe" -ArgumentList "/S"
+    Invoke-WebRequest -Uri "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" -OutFile "$nispoePath\SteamSetup.exe"
+    Start-Process -FilePath "$nispoePath\SteamSetup.exe" -ArgumentList "/S"
 }
 
 # Create place shortcuts for system startup
 $startupPath = "$HOME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 $shortcutURIPath = "https://raw.githubusercontent.com/nispoe/overclock/main/shortcuts"
-Invoke-WebRequest -Uri $shortcutURIPath"/3DMark.url" -OutFile $startupPath"\3DMark.url"
-Invoke-WebRequest -Uri $shortcutURIPath"/EVGA%20Precision%20X1.url" -OutFile $startupPath"\EVGA%20Precision%20X1.url"
+Invoke-WebRequest -Uri "$shortcutURIPath/3DMark.url" -OutFile "$startupPath\3DMark.url"
+Invoke-WebRequest -Uri "$shortcutURIPath/EVGA%20Precision%20X1.url" -OutFile "$startupPath\EVGA%20Precision%20X1.url"
