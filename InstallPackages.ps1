7f1742821b3754c3d6725ee095d7ec1ae5d6e6a9
nispoe <nispoe@eopsin.com>
2022-01-12 23:36:32 -0600

    update to install packages, decided to not automate steamcmd and specific apps for the moment

--- InstallPackages.ps1
+++ InstallPackages.ps1
@@ -2,6 +2,3 @@ $nispoepath = "C:\nispoe"
 New-Item -ItemType "directory" -Path $nispoepath
 Invoke-WebRequest -Uri "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" -OutFile $nispoepath"\SteamSetup.exe"
 Start-Process -FilePath $nispoepath"\SteamSetup.exe" -ArgumentList "/S"
-
-Invoke-WebRequest -Uri "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip" -OutFile $nispoepath"\steamcmd.zip"
-Expand-Archive $nispoepath"\steamcmd.zip" -DestinationPath $nispoepath"\steamcmd"
