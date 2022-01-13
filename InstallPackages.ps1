537757ec0026328872cfc7e296712cc4739b1719
nispoe <nispoe@eopsin.com>
2022-01-13 03:00:59 -0600

    added chocolatey and cpuz to install

--- InstallPackages.ps1
+++ InstallPackages.ps1
@@ -14,3 +14,10 @@ if (!(Test-Path "$nispoePath\SteamSetup.exe")) {
 $startupPath = "$HOME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 $shortcutURIPath = "https://raw.githubusercontent.com/nispoe/overclock/main/shortcuts"
 Invoke-WebRequest -Uri "$shortcutURIPath/3DMark.url" -OutFile "$startupPath\3DMark.url"
+
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Install CPUz
+choco install cpu-z.install
+
