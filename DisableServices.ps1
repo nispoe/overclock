7d0210784ba01d40f73b7f08033dcf3bc99bfcf9
nispoe <nispoe@eopsin.com>
2022-01-13 01:00:00 -0600

    update servicenames list in disable services script

--- DisableServices.ps1
+++ DisableServices.ps1
@@ -1,7 +1,21 @@
 $ServiceNames = @(
-    "Spooler"                           # Print Spooler
+    "AudioendpointBuilder"                          # Windows Audio EndpointBuilder
+    "Audiosrv"                                      # Windows Audio
+    "WinDefend"                                     # Microsoft Windows Defender Antivirus Service
+    "WdNisSvc"                                      # Microsoft Defender Antivirus Network Inspection Service
+    "wscsvc"                                        # Security Center
+    "Spooler"                                       # Print Spooler
+    "TabletInputService"                            # Touch Keyboard and Handwriting Panel pen and ink functionality
+    "DiagTrack"                                     # Connected User Experiences and Telemetry
+    "DusmSvc"                                       # Data Usage
+    "DoSvc"                                         # Delivery Optimization
+    "iphlpsvc"                                      # IP Hlper
+    "PcaSvc"                                        # Program Compatibility Assistant Service
+    "SecurityHealthService"                         # Windows Security Service
+    "Wcmsvc"                                        # Windows Connection Manager
 )
 
 foreach ($Service in $ServiceNames) {
+    Stop-Service -Name $Service -Force
     Set-Service -Name $Service -Status stopped -StartupType disabled
 }
