f99ddd966064595ac9613c5030722969b59ee047
nispoe <nispoe@eopsin.com>
2022-01-13 02:05:30 -0600

    adding shortcuts for apps to start at windows startup

--- InstallPackages.ps1
+++ InstallPackages.ps1
@@ -1,7 +1,11 @@
-$nispoepath = "C:\nispoe"
-New-Item -ItemType "directory" -Path $nispoepath
-Invoke-WebRequest -Uri "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" -OutFile $nispoepath"\SteamSetup.exe"
-Start-Process -FilePath $nispoepath"\SteamSetup.exe" -ArgumentList "/S"
+$nispoePath = "C:\nispoe"
+New-Item -ItemType "directory" -Path $nispoePath
+Invoke-WebRequest -Uri "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" -OutFile $nispoePath"\SteamSetup.exe"
+Start-Process -FilePath $nispoePath"\SteamSetup.exe" -ArgumentList "/S"
 
 # Create place shortcuts for startup
-Invoke-WebRequest -Uri ""
+$startupPath = $HOME"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
+$shortcutURIPath = "https://raw.githubusercontent.com/nispoe/overclock/main/shortcuts/"
+
+Invoke-WebRequest -Uri $shortcutURIPath"3DMark.url" -OutFile $startupPath"3DMark.url"
+Invoke-WebRequest -Uri $shortcutURIPath"EVGA%20Precision%20X1.url" -OutFile $startupPath"EVGA%20Precision%20X1.url"
