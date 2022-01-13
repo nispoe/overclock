662fcb65bdf40f032bbed222eee1fafaa0ba58de
nispoe <nispoe@eopsin.com>
2022-01-13 02:11:03 -0600

    correcting path

--- InstallPackages.ps1
+++ InstallPackages.ps1
@@ -4,8 +4,8 @@ Invoke-WebRequest -Uri "https://cdn.cloudflare.steamstatic.com/client/installer/
 Start-Process -FilePath $nispoePath"\SteamSetup.exe" -ArgumentList "/S"
 
 # Create place shortcuts for startup
-$startupPath = $HOME"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
-$shortcutURIPath = "https://raw.githubusercontent.com/nispoe/overclock/main/shortcuts/"
+$startupPath = $HOME"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+$shortcutURIPath = "https://raw.githubusercontent.com/nispoe/overclock/main/shortcuts"
 
-Invoke-WebRequest -Uri $shortcutURIPath"3DMark.url" -OutFile $startupPath"3DMark.url"
-Invoke-WebRequest -Uri $shortcutURIPath"EVGA%20Precision%20X1.url" -OutFile $startupPath"EVGA%20Precision%20X1.url"
+Invoke-WebRequest -Uri $shortcutURIPath"/3DMark.url" -OutFile $startupPath"\3DMark.url"
+Invoke-WebRequest -Uri $shortcutURIPath"/EVGA%20Precision%20X1.url" -OutFile $startupPath"\EVGA%20Precision%20X1.url"
