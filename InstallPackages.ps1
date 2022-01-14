0b4864fb92487c32b89d346fc268bfa681297143
nispoe <nispoe@eopsin.com>
2022-01-13 22:29:45 -0600

    added test path check for chocolatey

--- InstallPackages.ps1
+++ InstallPackages.ps1
@@ -1,5 +1,7 @@
 # Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+if (!(Test-Path "C:\ProgramData\chocolatey\choco.exe")) {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
 
 # Install Steam Client
 if (!(Test-Path "C:\Program Files (x86)\Steam\steam.exe")) {
