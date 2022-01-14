67d5d4206d7c4f38472719720978dc2d076f4ec5
nispoe <nispoe@eopsin.com>
2022-01-13 23:16:04 -0600

    syntax correction

--- JustRunItAll.ps1
+++ JustRunItAll.ps1
@@ -6,7 +6,7 @@ $scripts = @(
     "https://raw.githubusercontent.com/nispoe/overclock/main/ListOSInstalledPackages.ps1"
     "https://raw.githubusercontent.com/nispoe/overclock/main/ListUserInstalledPackages.ps1"
     "https://raw.githubusercontent.com/nispoe/overclock/main/ListRunningServices.ps1"
-}
+)
 
 foreach ($script in $scripts) {
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString($script))
