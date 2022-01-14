612de6c9f0172649fee7ff8a158c52b640298086
nispoe <nispoe@eopsin.com>
2022-01-13 22:07:13 -0600

    update installpackages to install latest nvidia driver

--- InstallPackages.ps1
+++ InstallPackages.ps1
@@ -5,12 +5,12 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 choco install steam-client --force -y
 choco install cpu-z.install --force -y
 choco install hwinfo --force -y
+choco install nvidia-display-driver --force -y
 
 # Add shortcuts to system startup
 $startupPath = "$HOME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 $shortcutURIPath = "https://raw.githubusercontent.com/nispoe/overclock/main/shortcuts"
 Invoke-WebRequest -Uri "$shortcutURIPath/3DMark.url" -OutFile "$startupPath\3DMark.url"
-
 
 # Add shorcuts to system desktop
 Invoke-WebRequest -Uri "$shortcutURIPath/HWiNFO64.lnk" -OutFile "$HOME\Desktop\HWiNFO64.lnk"
