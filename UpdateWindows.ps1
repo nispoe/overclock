338021556f1b648046eca3c19b9a369a31e7f5ef
nispoe <nispoe@eopsin.com>
2022-01-13 23:30:32 -0600

    remove reboot

--- UpdateWindows.ps1
+++ UpdateWindows.ps1
@@ -2,4 +2,4 @@ Install-PackageProvider NuGet -Force
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 Install-Module PSWindowsUpdate -Repository PSGallery
 Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force
-Get-WindowsUpdate -AcceptAll -Install -AutoReboot
+Get-WindowsUpdate -AcceptAll -Install
