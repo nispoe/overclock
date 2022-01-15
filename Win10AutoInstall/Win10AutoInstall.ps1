b40fee659ed9d78427c7c4075a621e2f4ab4e696
nispoe <nispoe@eopsin.com>
2022-01-15 06:02:20 -0600

    made all copy show progress

--- Win10AutoInstall/Win10AutoInstall.ps1
+++ Win10AutoInstall/Win10AutoInstall.ps1
@@ -14,11 +14,11 @@ if (!(Test-Path $nispoePath)) {
 if (Test-Path "$driveLetter`:\sources") {
     # Found the generation of the wim process faster on disk so copy to USB after file is generated.
     # Copy install.esd to workind directory
-    Copy-Item "$driveLetter`:\sources\install.esd" -Destination "$nispoePath"
+    Start-BitsTransfer "$driveLetter`:\sources\install.esd" -Destination "$nispoePath" -Description "$driveLetter`:\sources\install.esd to $nispoePath\install.esd" -DisplayName "Copying file"
     # Generate the install.wim file under sources directory
     dism /Export-Image /SourceImageFile:"$nispoePath\install.esd" /SourceIndex:6 /DestinationImageFile:"$nispoePath\install.wim" /CheckIntegrity
     # Copy install.wim to USB drive
-    Start-BitsTransfer -Source "$nispoePath\install.wim" -Destination "$driveLetter`:\sources\" -Description "$nispoePath\install.wim to $driveLetter`:\sources\install.wim" -DisplayName "Copying wim file"
+    Start-BitsTransfer -Source "$nispoePath\install.wim" -Destination "$driveLetter`:\sources\" -Description "$nispoePath\install.wim to $driveLetter`:\sources\install.wim" -DisplayName "Copying file"
 
     # Copy autounattend.xml file to nispoe working directory to modify
     $shortcutURIPath = "https://raw.githubusercontent.com/nispoe/overclock/main/Win10AutoInstall"
@@ -30,7 +30,7 @@ if (Test-Path "$driveLetter`:\sources") {
     (Get-Content -path $autounattendFile -Raw) -replace "nispoe","$loginName" | Set-Content -Path $autounattendFile
 
     # Copy autoattend.xml and install_Windows 10 Pro.clg files to USB drive
-    Copy-Item $autounattendFile -Destination "$driveLetter`:\"
+    Start-BitsTransfer "$autounattendFile" -Destination "$driveLetter`:\" -Description "$autounattendFile\install_Windows 10 Pro.clg to $driveLetter`:\install_Windows 10 Pro.clg" -DisplayName "Copying file"
     Invoke-WebRequest -Uri "$shortcutURIPath/install_Windows 10 Pro.clg" -OutFile "$driveLetter`:\sources\install_Windows 10 Pro.clg"
 
     # Cleanup nispoe working directory
