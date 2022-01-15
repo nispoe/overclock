c2d152568856a908f5dbb5267e19ca9da030f8df
nispoe <nispoe@eopsin.com>
2022-01-15 05:49:01 -0600

    moved code into the drive check

--- Win10AutoInstall/Win10AutoInstall.ps1
+++ Win10AutoInstall/Win10AutoInstall.ps1
@@ -19,23 +19,22 @@ if (Test-Path "$driveLetter`:\sources") {
     dism /Export-Image /SourceImageFile:"$nispoePath\install.esd" /SourceIndex:6 /DestinationImageFile:"$nispoePath\install.wim" /CheckIntegrity
     # Copy install.wim to USB drive
     Start-BitsTransfer -Source "$nispoePath\install.wim" -Destination "$driveLetter`:\sources\" -Description "Copying wim file" -DisplayName "Copy"
+
+    # Copy autounattend.xml file to nispoe working directory to modify
+    $shortcutURIPath = "https://raw.githubusercontent.com/nispoe/overclock/main/Win10AutoInstall"
+    Invoke-WebRequest -Uri "$shortcutURIPath/autounattend.xml" -OutFile "$nispoePath\autounattend.xml"
+
+    # Replace Computer Name and Login Name
+    $autounattendFile = "$nispoePath\autounattend.xml"
+    (Get-Content -path $autounattendFile -Raw) -replace "eopsin","$computerName" | Set-Content -Path $autounattendFile
+    (Get-Content -path $autounattendFile -Raw) -replace "nispoe","$loginName" | Set-Content -Path $autounattendFile
+
+    # Copy autoattend.xml and install_Windows 10 Pro.clg files to USB drive
+    Copy-Item $autounattendFile -Destination "$driveLetter`:\"
+    Invoke-WebRequest -Uri "$shortcutURIPath/install_Windows 10 Pro.clg" -OutFile "$driveLetter`:\sources\install_Windows 10 Pro.clg"
+
+    # Cleanup nispoe working directory
+    Remove-Item -LiteralPath $nispoePath -Force -Recurse
 } else {
     Write-Output "Drive letter does not look right, try running again."
 }
-
-# Copy autounattend.xml file to nispoe working directory to modify
-$shortcutURIPath = "https://raw.githubusercontent.com/nispoe/overclock/main/Win10AutoInstall"
-Invoke-WebRequest -Uri "$shortcutURIPath/autounattend.xml" -OutFile "$nispoePath\autounattend.xml"
-
-# Replace Computer Name and Login Name
-$autounattendFile = "$nispoePath\autounattend.xml"
-(Get-Content -path $autounattendFile -Raw) -replace "eopsin","$computerName" | Set-Content -Path $autounattendFile
-(Get-Content -path $autounattendFile -Raw) -replace "nispoe","$loginName" | Set-Content -Path $autounattendFile
-
-# Copy autoattend.xml and install_Windows 10 Pro.clg files to USB drive
-Copy-Item $autounattendFile -Destination "$driveLetter`:\"
-Invoke-WebRequest -Uri "$shortcutURIPath/install_Windows 10 Pro.clg" -OutFile "$driveLetter`:\sources\install_Windows 10 Pro.clg"
-
-# Cleanup nispoe working directory
-Remove-Item -LiteralPath $nispoePath -Force -Recurse
-
