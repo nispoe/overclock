85b78c6dcd78046d0d911adaeb6a77cb77a89483
nispoe <nispoe@eopsin.com>
2022-01-17 16:41:23 -0600

    added custom desktop background and chrome

--- PoPOS/InstallPersonalPackages.sh
+++ PoPOS/InstallPersonalPackages.sh
@@ -3,8 +3,17 @@ sudo add-apt-repository multiverse -y
 sudo apt update -y
 sudo apt upgrade -y
 
+# set eopsinDesktop background
+wget "https://raw.githubusercontent.com/nispoe/overclock/main/images/eopsinDesktop.png" "${HOME}/Pictures"
+gsettings set org.gnome.desktop.background picture-uri "file:///${HOME}/Pictures/eopsinDesktop.png"
+
 # install steam
 sudo apt install steam -y
 
 # install Visual Studio Code
 sudo apt install code -y
+
+# install Google Chrome
+sudo apt-get install libxss1 libappindicator1 libindicator7
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome*.deb -y
