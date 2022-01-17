c3fa9575cce6f224968250fdd5b885619f72f4b2
nispoe <nispoe@eopsin.com>
2022-01-17 15:49:23 -0600

    added steam and visual studio code to install personal packages script

--- PoPOS/InstallPersonalPackages.sh
+++ PoPOS/InstallPersonalPackages.sh
@@ -1,3 +1,10 @@
-sudo add-apt-repository multiverse
-sudo apt update
-sudo apt install steam
+# adding multiverse repository
+sudo add-apt-repository multiverse -y
+sudo apt update -y
+sudo apt upgrade -y
+
+# install steam
+sudo apt install steam -y
+
+# install Visual Studio Code
+sudo apt install code -y
