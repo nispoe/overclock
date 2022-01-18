9def5aad7035ea62360a9398eb6ad6407c648ff7
nispoe <nispoe@eopsin.com>
2022-01-18 03:50:31 -0600

    trying again with wget

--- PoPOSInstall/InstallPersonalPackages.sh
+++ PoPOSInstall/InstallPersonalPackages.sh
@@ -4,7 +4,7 @@ sudo apt update -y
 sudo apt upgrade -y
 
 # set eopsinDesktop background
-wget -O ~/Pictures/eopsinDesktop.png https://raw.githubusercontent.com/nispoe/overclock/main/images/eopsinDesktop.png
+wget -O "${HOME}/Pictures/eopsinDesktop.png" "https://raw.githubusercontent.com/nispoe/overclock/main/images/eopsinDesktop.png"
 gsettings set org.gnome.desktop.background picture-uri "file:///${HOME}/Pictures/eopsinDesktop.png"
 
 # install steam
@@ -15,5 +15,5 @@ sudo apt install code -y
 
 # install Google Chrome
 sudo apt-get install libxss1 libappindicator1 libindicator7
-sudo wget -O ~/Downloads/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo wget -O "${HOME}/Downloads/google-chrome-stable_current_amd64.deb" "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 sudo apt install "${HOME}/Downloads/google-chrome-stable_current_amd64.deb -y"
