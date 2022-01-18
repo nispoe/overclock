fdb8b3454289ba21921ffb5ae8fc51a57f62db75
nispoe <nispoe@eopsin.com>
2022-01-18 03:30:45 -0600

    correcting script for home Pictures and Downloads paths

--- PoPOSInstall/InstallPersonalPackages.sh
+++ PoPOSInstall/InstallPersonalPackages.sh
@@ -4,7 +4,7 @@ sudo apt update -y
 sudo apt upgrade -y
 
 # set eopsinDesktop background
-wget "https://raw.githubusercontent.com/nispoe/overclock/main/images/eopsinDesktop.png" "${HOME}/Pictures"
+wget "https://raw.githubusercontent.com/nispoe/overclock/main/images/eopsinDesktop.png" "${HOME}/Pictures/"
 gsettings set org.gnome.desktop.background picture-uri "file:///${HOME}/Pictures/eopsinDesktop.png"
 
 # install steam
@@ -15,5 +15,5 @@ sudo apt install code -y
 
 # install Google Chrome
 sudo apt-get install libxss1 libappindicator1 libindicator7
-sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb "${HOME}/Downloads"
-sudo apt install "./Downloads/google-chrome*.deb -y"
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb "${HOME}/Downloads/"
+sudo apt install "${HOME}/Downloads/google-chrome*.deb -y"
