8de6b4f14851a919f470788a044a2c9d56f5f653
nispoe <nispoe@eopsin.com>
2022-01-18 04:09:29 -0600

    updated synatx error with apt install of google chrome

--- PoPOSInstall/InstallPersonalPackages.sh
+++ PoPOSInstall/InstallPersonalPackages.sh
@@ -5,7 +5,7 @@ sudo apt upgrade -y
 
 # set eopsinDesktop background
 wget -O "${HOME}/Pictures/eopsinDesktop.png" "https://raw.githubusercontent.com/nispoe/overclock/main/images/eopsinDesktop.png"
-gsettings set org.gnome.desktop.background picture-uri "file:///${HOME}/Pictures/eopsinDesktop.png"
+gsettings set org.gnome.desktop.background picture-uri "file://${HOME}/Pictures/eopsinDesktop.png"
 
 # install steam
 sudo apt install steam -y
@@ -16,4 +16,4 @@ sudo apt install code -y
 # install Google Chrome
 sudo apt-get install libxss1 libappindicator1 libindicator7
 sudo wget -O "${HOME}/Downloads/google-chrome-stable_current_amd64.deb" "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-sudo apt install "${HOME}/Downloads/google-chrome-stable_current_amd64.deb -y"
+sudo apt install "${HOME}/Downloads/google-chrome-stable_current_amd64.deb" -y
