c9f920c613ecb6d38118ddaad7f5da9d62ee0405
nispoe <nispoe@eopsin.com>
2022-01-21 21:53:53 -0800

    updated comments used for creating link, removed noexit parameter

--- toolbox/Windows/OpenMemoryApps.ps1
+++ toolbox/Windows/OpenMemoryApps.ps1
@@ -1,5 +1,5 @@
 # Shortcut Created
-# powershell.exe -noexit -ExecutionPolicy Bypass -File "C:\nispoe\toolbox\Windows\OpenMemoryApps.ps1"
+# powershell.exe -ExecutionPolicy Bypass -File "C:\nispoe\toolbox\Windows\OpenMemoryApps.ps1"
 
 Invoke-Expression "C:\Progra~1\CPUID\CPU-Z\cpuz.exe"
 Invoke-Expression "C:\Progra~1\CPUID\CPU-Z\cpuz.exe"
