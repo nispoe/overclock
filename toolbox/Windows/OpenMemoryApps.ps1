5f53c56ea722b614b80d345f12429cc730537dec
nispoe <nispoe@eopsin.com>
2022-01-22 01:54:12 -0600

    changed cpuz to loop and open 5 times

--- toolbox/Windows/OpenMemoryApps.ps1
+++ toolbox/Windows/OpenMemoryApps.ps1
@@ -1,9 +1,9 @@
 # Shortcut Created
 # powershell.exe -ExecutionPolicy Bypass -File "C:\nispoe\toolbox\Windows\OpenMemoryApps.ps1"
 
-Invoke-Expression "C:\Progra~1\CPUID\CPU-Z\cpuz.exe"
-Invoke-Expression "C:\Progra~1\CPUID\CPU-Z\cpuz.exe"
-Invoke-Expression "C:\Progra~1\CPUID\CPU-Z\cpuz.exe"
-Invoke-Expression "C:\Progra~1\CPUID\CPU-Z\cpuz.exe"
+for ($i=1; $i -le 5; $i++) {
+    Invoke-Expression "C:\Progra~1\CPUID\CPU-Z\cpuz.exe"
+}
+
 Invoke-Expression "C:\Users\nispoe\AppData\Local\Programs\BenchMate\BenchMate.exe"
 Invoke-Expression "& 'C:\Program Files (x86)\FinalWire\AIDA64 Extreme\aida64.exe'"
