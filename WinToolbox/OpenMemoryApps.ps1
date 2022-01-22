39c17955ae9e822fa84a0332dcba0fbc194a7f47
nispoe <nispoe@eopsin.com>
2022-01-21 18:53:06 -0800

    made script to open all the apps used during memory overclocking

---
+++ WinToolbox/OpenMemoryApps.ps1
@@ -0,0 +1,6 @@
+Invoke-Expression "C:\Progra~1\CPUID\CPU-Z\cpuz.exe"
+Invoke-Expression "C:\Progra~1\CPUID\CPU-Z\cpuz.exe"
+Invoke-Expression "C:\Progra~1\CPUID\CPU-Z\cpuz.exe"
+Invoke-Expression "C:\Progra~1\CPUID\CPU-Z\cpuz.exe"
+Invoke-Expression "C:\Users\nispoe\AppData\Local\Programs\BenchMate\BenchMate.exe"
+Invoke-Expression "& 'C:\Program Files (x86)\FinalWire\AIDA64 Extreme\aida64.exe'"
