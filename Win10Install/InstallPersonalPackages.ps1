6e7ee5b10d351036cd984037d9de0c70e40170f0
nispoe <nispoe@eopsin.com>
2022-01-21 21:52:43 -0800

    corrected shortcuts and directory correction for toolbox items

--- Win10Install/InstallPersonalPackages.ps1
+++ Win10Install/InstallPersonalPackages.ps1
@@ -52,8 +52,9 @@ Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Pe
 Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
 
 # Copy toolbox items
-$nispeToolboxPath = "C:\nispoe\toolbox\"
-Invoke-WebRequest -Uri "$githubURIPath/toolbox/Windows/EnableStartupApps.ps1" -OutFile "$nispeToolboxPath\Windows\EnableStartupApps.ps1"
+$nispeToolboxPath = "C:\nispoe\toolbox\Windows"
+New-Item $nispeToolboxPath -ItemType Directory
+Invoke-WebRequest -Uri "$githubURIPath/toolbox/Windows/EnableStartupApps.ps1" -OutFile "$nispeToolboxPath\EnableStartupApps.ps1"
 Invoke-WebRequest -Uri "$githubURIPath/Win10Install/shortcuts/EnableStartupApps - Shortcut.lnk" -OutFile "$HOME\Desktop\EnableStartupApps - Shortcut.lnk"
-Invoke-WebRequest -Uri "$githubURIPath/toolbox/Windows/OpenMemoryApps.ps1" -OutFile "$nispeToolboxPath\Windows\OpenMemoryApps.ps1"
+Invoke-WebRequest -Uri "$githubURIPath/toolbox/Windows/OpenMemoryApps.ps1" -OutFile "$nispeToolboxPath\OpenMemoryApps.ps1"
 Invoke-WebRequest -Uri "$githubURIPath/Win10Install/shortcuts/OpenMemoryApps - Shortcut.lnk" -OutFile "$HOME\Desktop\OpenMemoryApps - Shortcut.lnk"
