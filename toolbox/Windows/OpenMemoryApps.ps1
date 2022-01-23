c05a5da7dd0c02247bc82d33efe5ad6dc736c4a2
nispoe <nispoe@eopsin.com>
2022-01-23 00:41:33 -0600

    update shortcut startup paths

--- toolbox/Windows/OpenMemoryApps.ps1
+++ toolbox/Windows/OpenMemoryApps.ps1
@@ -2,10 +2,10 @@
 # powershell.exe -ExecutionPolicy Bypass -File "C:\nispoe\toolbox\OpenMemoryApps.ps1"
 
 for ($i=1; $i -le 5; $i++) {
-    Invoke-Expression "& 'C:\nispoe\toolbox\Windows\CPUID CPU-Z.lnk'"
+    Invoke-Expression "& 'C:\nispoe\toolbox\CPUID CPU-Z.lnk'"
 }
 
-Invoke-Expression "& 'C:\nispoe\toolbox\Windows\HWiNFO64.lnk'"
-Invoke-Expression "& 'C:\nispoe\toolbox\Windows\LinpackXtreme_x64'"
-Invoke-Expression "& 'C:\nispoe\toolbox\Windows\BenchMate.lnk'"
-Invoke-Expression "& 'C:\nispoe\toolbox\Windows\AIDA64 Extreme.lnk'"
+Invoke-Expression "& 'C:\nispoe\toolbox\HWiNFO64.lnk'"
+Invoke-Expression "& 'C:\nispoe\toolbox\LinpackXtreme_x64'"
+Invoke-Expression "& 'C:\nispoe\toolbox\BenchMate.lnk'"
+Invoke-Expression "& 'C:\nispoe\toolbox\AIDA64 Extreme.lnk'"
