cdedf043627817156a045b88714bd17f7290fd72
nispoe <nispoe@eopsin.com>
2022-01-25 17:55:15 -0600

    split out into two scripts packages to install one for personal and another for a developer environment

--- PoPOSInstall/InstallDeveloperPackages.sh
+++ PoPOSInstall/InstallDeveloperPackages.sh
@@ -7,10 +7,5 @@ sudo apt-get install libxss1 libappindicator1 libindicator7
 sudo wget -O "${HOME}/Downloads/google-chrome-stable.deb" "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 sudo apt install "${HOME}/Downloads/google-chrome-stable.deb" -y
 
-# adding multiverse repository
-sudo add-apt-repository multiverse -y
-sudo apt update -y
-sudo apt upgrade -y
-
-# install steam
-sudo apt install steam -y
+# install Visual Studio Code
+sudo apt install code -y
